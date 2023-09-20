eeb08c25cb66c7a8580f192e6d36fec83bdadcca
Herison Pereira <herisonup@gmail.com>
2023-09-20 00:27:20 -0300

    First commit

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Tribo Business | Teste',
+  description: 'De mulas que querem vender, para mulas que querem comprar',
+}
+
+export default function Teste() {
+    return (
+      <button>
+        Botão de teste
+      </button>
+    )
+  }
+  
